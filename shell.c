3f783dbeb1a0a708a1c8eab00cec4d86ba98ae6f
harryggg <ma.tanghao@dhs.sg>
2016-02-12 01:41:29 +0800

    implement command exec

--- shell.c
+++ shell.c
@@ -2,14 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 void processCommand(char* command);
 
 int main(){
 	char command[999];
+	
 	while(1){
 		printf("> ");
 		fgets(command, sizeof(command), stdin);
+		if(command[strlen(command)-1] == '\n') {
+            command[strlen(command)-1] = '\0';
+        }
 		processCommand(command);
 	}
 }
@@ -19,8 +24,21 @@ void processCommand(char* command){
 	pid_t pid = fork();
 	if (pid == 0) { //child process
 		printf("Parent pid: %i\n",getppid());
+		char *p;
+		char *params[15];
+		int count = 0;
+		p = strtok(command, " ");
+		params[count++] = p;
+		while (p != NULL) {
+			p = strtok(NULL, " ");
+  			params[count++] = p;
+  		}
+        execvp(params[0],params);
+        return;
+
 	}else{
 		printf("Loading new process with id %i\n",pid);
+		int status;
+	//	waitpid(pid,&status,0);
 	}
-
 }
